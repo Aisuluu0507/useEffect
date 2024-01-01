5339b6d4f771d9843ac0b1dc1b643081d833f9a7
Aisuluu0507 <aisuulu0507@gmail.com>
2024-01-01 20:20:23 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import YourComponent from './App';
+import TextChanger from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <YourComponent/>
+    <TextChanger/>
   </React.StrictMode>
 );
 
